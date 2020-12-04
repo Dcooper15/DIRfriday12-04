ba6e90dc23db7e3a4ecc97f90716d4ade355a102
Dylan Cooper <dylancooper155@gmail.com>
2020-12-04 10:23:30 -0500

    boilerplate

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-const url = 'https://dog.ceo/api/breeds/image/random/3';
+const url = '';
 const randomDogsElement = document.querySelector('.random-dogs');
 const loadingElement = document.querySelector('.loading');
 const goButton = document.querySelector('.go-button');
@@ -6,32 +6,27 @@ const goButton = document.querySelector('.go-button');
 loadingElement.style.display = 'none';
 
 async function getRandomDogs() {
-  const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
-  
-  
-  json.message.forEach(dogImage => {
-    randomDogsElement.innerHTML += `
-    <div class="column">
-      <div class="card">
-        <div class="card-image">
-          <figure class="image">
-            <img src="${dogImage}" alt="Placeholder image">
-          </figure>
-        </div>
-      </div>
-    </div>
-    `;
  
-  });
-  loadingElement.style.display = 'none';
-}
+  
+  
+//   json.message.forEach(dogImage => {
+//     randomDogsElement.innerHTML += `
+//     <div class="column">
+//       <div class="card">
+//         <div class="card-image">
+//           <figure class="image">
+//             <img src="${dogImage}" alt="Placeholder image">
+//           </figure>
+//         </div>
+//       </div>
+//     </div>
+//     `;
+ 
+//   });
+//   loadingElement.style.display = 'none';
+ }
 
 goButton.addEventListener('click', getRandomDogs);
 
 
 
-//     const response = await fetch(url);
-//   const json = await response.json();
-//   console.log(json);
